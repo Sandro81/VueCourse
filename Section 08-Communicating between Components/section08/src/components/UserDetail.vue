7fff3689f5f6b457e6160a110dceb5dda95444c5
Sandro81 <sandro.fioravanti81@gmail.com>
2019-10-27 12:26:29 +0000

    Section 08

--- Section 08-Communicating between Components/section08/src/components/UserDetail.vue
+++ Section 08-Communicating between Components/section08/src/components/UserDetail.vue
@@ -3,17 +3,23 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{switchName()}}</p>
-        <button @click="resetName">Reset</button>
+        <p>User Name: {{userAge}}</p>
+        <button @click="resetName">resetName</button>
+        <button @click="resetFn()">resetFn</button>
     </div>
 </template>
 
 <script>
+    import { eventBus } from "@/main";
+
     export default {
         props: {
             myName: {
                 type: String,
                 required: true,
-            }
+            },
+            resetFn: Function,
+            userAge: Number,
         },
         methods: {
             switchName() {
@@ -23,6 +29,11 @@
                 this.myName = 'Sandro';
                 this.$emit('nameWasReset', this.myName);
             }
+        },
+        created() {
+            eventBus.$on('ageWasEdited', (age)=>{
+                this.userAge = age;
+            });
         }
     }
 </script>
