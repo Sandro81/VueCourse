1d462a4f3b8689580ba050e45660b798bdc39c1b
Sandro81 <sandro.fioravanti81@gmail.com>
2019-10-27 09:50:59 +0000

    Assignment 6: Time to Practice - Components

---
+++ Section 07-An Introduction to Components/assignment6_components/src/components/ServerList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <app-server></app-server>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ServerList"
+    }
+</script>
+
+<style scoped>
+
+</style>
