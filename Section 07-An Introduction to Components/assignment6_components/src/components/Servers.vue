dae863b8776fece43545e1ee2e5b00bfb8e295b7
Sandro81 <sandro.fioravanti81@gmail.com>
2019-10-27 10:18:07 +0000

    Assignment 6: Time to Practice - Components

--- Section 07-An Introduction to Components/assignment6_components/src/components/Servers.vue
+++ Section 07-An Introduction to Components/assignment6_components/src/components/Servers.vue
@@ -1,5 +1,13 @@
 <template>
-    
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li class="list-group-item"
+                    v-for="index in 5"
+                v-bind:key="index.id">
+                Server #{{ index }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
