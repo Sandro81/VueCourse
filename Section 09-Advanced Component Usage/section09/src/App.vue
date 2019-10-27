486f13abdd95b3ebd99e7a378e6ba750d0ac3b51
Sandro81 <sandro.fioravanti81@gmail.com>
2019-10-27 18:46:26 +0000

    Section09

--- Section 09-Advanced Component Usage/section09/src/App.vue
+++ Section 09-Advanced Component Usage/section09/src/App.vue
@@ -2,6 +2,10 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
+        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <button @click="selectedComponent = 'appAuthor'">Author</button>
+        <button @click="selectedComponent = 'appNew'">New</button>
+        <hr>
         <app-quote>
           <h1 slot="title">{{quoteTitle}}</h1>
           <p slot="content">A Wonderful Quote</p>
@@ -13,14 +17,19 @@
 
 <script>
   import Quote from "@/components/Quote";
+  import Author from "@/components/Author";
+  import New from "@/components/New";
   export default {
       data: function () {
         return{
-          quoteTitle: 'The Quote'
+          quoteTitle: 'The Quote',
+          selectedComponent: 'appQuote'
         }
       },
     components: {
-      appQuote: Quote
+      appQuote: Quote,
+      appAuthor: Author,
+      appNew: New
     }
   }
 </script>
