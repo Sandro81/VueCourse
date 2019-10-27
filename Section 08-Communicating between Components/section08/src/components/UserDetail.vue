1236e8cf24242b708b0cffaa4942e2e80b0d23d6
Sandro81 <sandro.fioravanti81@gmail.com>
2019-10-27 12:01:09 +0000

    Section 08

---
+++ Section 08-Communicating between Components/section08/src/components/UserDetail.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{switchName()}}</p>
+        <button @click="resetName">Reset</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            myName: {
+                type: String,
+                required: true,
+            }
+        },
+        methods: {
+            switchName() {
+                return this.myName.split("").reverse().join("");
+            },
+            resetName() {
+                this.myName = 'Sandro';
+                this.$emit('nameWasReset', this.myName);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
