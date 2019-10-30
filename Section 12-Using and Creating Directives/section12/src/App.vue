ad93fe2756225f55be3a6f24304c5827515b395b
Sandro81 <sandro.fioravanti81@gmail.com>
2019-10-30 20:57:34 +0000

    Section12

--- Section 12-Using and Creating Directives/section12/src/App.vue
+++ Section 12-Using and Creating Directives/section12/src/App.vue
@@ -4,6 +4,11 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Directives</h1>
         <p v-text="'Some Text'"></p>
+        <p v-html="'<strong>Some Strong Text</strong>'"></p>
+      </div>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p style="">Color this</p>
       </div>
     </div>
   </div>
