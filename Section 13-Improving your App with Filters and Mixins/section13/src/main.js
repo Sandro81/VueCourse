795015ef89b4f965fd9ecc9f606b4e0996c362f6
Sandro81 <sandro.fioravanti81@gmail.com>
2019-10-31 20:21:04 +0000

    section13

--- Section 13-Improving your App with Filters and Mixins/section13/src/main.js
+++ Section 13-Improving your App with Filters and Mixins/section13/src/main.js
@@ -5,6 +5,14 @@ Vue.config.productionTip = false
 Vue.filter('to-lowerCaseGloballyFilter', function (value) {
     return value.toLowerCase();
 });
+
+Vue.mixin({
+  created() {
+    /*eslint no-console: "error"*/
+// custom console
+    Console.log('Global Mixin - Created Hook');
+  }
+});
 new Vue({
   render: h => h(App),
 }).$mount('#app')
