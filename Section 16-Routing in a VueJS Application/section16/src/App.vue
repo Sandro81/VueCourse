be6ece7a41c09afe5e0a2f5a44b66203b4a7eb5f
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-02 17:53:42 +0000

    section16

--- Section 16-Routing in a VueJS Application/section16/src/App.vue
+++ Section 16-Routing in a VueJS Application/section16/src/App.vue
@@ -1,7 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <img alt="Vue logo" src="./assets/logo.png" style="width: 50px; height: auto;">
+        </div>
+        <div class="w-100"></div>
+        <div class="col">
+          <h1>Section 16: Routing in a VueJS Application</h1>
+          <p>npm install --save vue-router</p>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+    <div class="container">
+      <div class="row">
+        <div class="col">col</div>
+        <div class="col">col</div>
+        <div class="w-100"></div>
+        <div class="col">col</div>
+        <div class="col">col</div>
+      </div>
+    </div>
   </div>
 </template>
 
