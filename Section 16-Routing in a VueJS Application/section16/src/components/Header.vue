abfe22ded005f0c86f43aa6be30ad3c7ff7aa1d4
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-02 18:53:52 +0000

    section16

--- Section 16-Routing in a VueJS Application/section16/src/components/Header.vue
+++ Section 16-Routing in a VueJS Application/section16/src/components/Header.vue
@@ -6,17 +6,12 @@
             </a>
             <ul class="nav nav-pills">
                 <li class="nav-item">
-                    <router-link class="nav-link active" to="/">Home</router-link>
+                    <router-link  class="nav-link" to="/" active-class="active">Home</router-link>
                 </li>
                 <li class="nav-item">
-                    <router-link class="nav-link" to="/user">User</router-link>
+                    <router-link  class="nav-link" to="/user" active-class="active">User</router-link>
                 </li>
             </ul>
-
-
-
-
-
         </nav>
 </template>
 
