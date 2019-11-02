4996ae1dc03364e7f5bfa0e581625955a0356189
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-02 17:30:34 +0000

    section15

--- Section 15-Connecting to Servers via Http - Using vue-resource/section15/src/main.js
+++ Section 15-Connecting to Servers via Http - Using vue-resource/section15/src/main.js
@@ -1,23 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource';
-import response from "vue-resource/src/http/response";
 
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(VueResource);
-Vue.http.options.root = 'https://vuehttptest-f8e41.firebaseio.com/data.json';
+Vue.http.options.root = 'https://vuehttptest-f8e41.firebaseio.com/';
 Vue.http.interceptors.push((request, next) => {
   console.log(request);
   if(request.method == 'POST') {
     request.method = 'PUT';
   }
-  next(response => {
-    response.json = () => {
-      return { messages: response.body}
-    }
-  });
+  next();
+  // next(response => {
+  //   response.json = () => {
+  //     return { messages: response.body}
+  //   }
+  // });
 });
 new Vue({
   render: h => h(App),
