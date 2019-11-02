2d7d5444796c113d069b3a100bb3c2a16c15d472
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-02 20:36:49 +0000

    section16

--- Section 16-Routing in a VueJS Application/section16/src/App.vue
+++ Section 16-Routing in a VueJS Application/section16/src/App.vue
@@ -10,21 +10,24 @@
         </div>
         <div class="w-100"></div>
         <div class="col">
-          <router-view></router-view>
+          <transition name="slide">
+            <router-view></router-view>
+          </transition>
+
         </div>
       </div>
     </div>
 
-<!--    <hr>-->
-<!--    <div class="container">-->
-<!--      <div class="row">-->
-<!--        <div class="col">col</div>-->
-<!--        <div class="col">col</div>-->
-<!--        <div class="w-100"></div>-->
-<!--        <div class="col">col</div>-->
-<!--        <div class="col">col</div>-->
-<!--      </div>-->
-<!--    </div>-->
+    <!--    <hr>-->
+    <!--    <div class="container">-->
+    <!--      <div class="row">-->
+    <!--        <div class="col">col</div>-->
+    <!--        <div class="col">col</div>-->
+    <!--        <div class="w-100"></div>-->
+    <!--        <div class="col">col</div>-->
+    <!--        <div class="col">col</div>-->
+    <!--      </div>-->
+    <!--    </div>-->
     <router-view name="header-bottom"></router-view>
   </div>
 
@@ -32,22 +35,84 @@
 
 <script>
 
-import Header from "@/components/Header";
-
-export default {
-  name: 'app',
-  components: {
-    appHeader: Header
+  export default {
+    name: 'app',
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  .col {
+    min-height: 70px;
+
+  }
+  .fade-enter {
+    opacity: 0;
+  }
+
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+
+  .fade-leave {
+    /*opacity: 1;*/
+  }
+
+  .fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+  }
+
+  .slide-enter {
+    opacity: 0;
+    /*transform: translateY(20px);*/
+  }
+
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity .5s;
+  }
+
+  .slide-leave {
+
+  }
+
+  .slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+    transition: opacity 1s;
+    opacity: 0;
+    position: absolute;
+  }
+
+
+  .slide-move {
+    transition: transform 1s;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(20px);
+    }
+  }
+
+
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
--- Section 16-Routing in a VueJS Application/section16/src/router.js
+++ Section 16-Routing in a VueJS Application/section16/src/router.js
@@ -3,14 +3,23 @@ import Home from "@/components/Home";
 import UserStart from "@/components/user/UserStart";
 import UserDetail from "@/components/user/UserDetail";
 import UserEdit from "@/components/user/UserEdit";
+import Header from "@/components/Header";
 
 
 export const routes = [
-    {path: '', component: Home, name: 'home'},
-    {path: '/user', component: User, children: [
+    {path: '', name: 'home', components: {
+            default: Home,
+            'header-top': Header,
+        }},
+    {path: '/user', children: [
             {path: '', component: UserStart},
             {path: ':id', component: UserDetail},
             {path: ':id/edit', component: UserEdit, name: 'userEdit'},
-        ]},
+        ], components: {
+            default: User,
+            'header-bottom': Header,
+        }},
+    {path: 'redirect-me', redirect: '/user'},
+    {path: '*', redirect: '/'}
 
 ];
