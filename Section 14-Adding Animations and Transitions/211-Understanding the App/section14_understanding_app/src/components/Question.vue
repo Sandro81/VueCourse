80fe84608bcf8a26e08d6bf8ddbc71839190e1d5
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-02 12:14:42 +0000

    section14

--- Section 14-Adding Animations and Transitions/211-Understanding the App/section14_understanding_app/src/components/Question.vue
+++ Section 14-Adding Animations and Transitions/211-Understanding the App/section14_understanding_app/src/components/Question.vue
@@ -1,23 +1,25 @@
 <template>
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title text-center">{{ question }}</h3>
-        </div>
-        <div class="panel-body">
-            <div class="col-xs-12 col-sm-6 text-center">
-                <button class="btn btn-primary btn-lg" style="margin: 10px" @click="onAnswer(btnData[0].correct)">{{ btnData[0].answer }}</button>
-            </div>
-            <div class="col-xs-12 col-sm-6 text-center">
-                <button class="btn btn-primary btn-lg" style="margin: 10px" @click="onAnswer(btnData[1].correct)">{{ btnData[1].answer }}</button>
-            </div>
-            <div class="col-xs-12 col-sm-6 text-center">
-                <button class="btn btn-primary btn-lg" style="margin: 10px" @click="onAnswer(btnData[2].correct)">{{ btnData[2].answer }}</button>
-            </div>
-            <div class="col-xs-12 col-sm-6 text-center">
-                <button class="btn btn-primary btn-lg" style="margin: 10px" @click="onAnswer(btnData[3].correct)">{{ btnData[3].answer }}</button>
+
+
+    <div class="card">
+        <h5 class="card-header">{{ question }}</h5>
+        <div class="card-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col"><button class="btn btn-primary btn-lg" style="margin: 10px" @click="onAnswer(btnData[0].correct)">{{ btnData[0].answer }}</button>
+                    </div>
+                    <div class="col"> <button class="btn btn-primary btn-lg" style="margin: 10px" @click="onAnswer(btnData[1].correct)">{{ btnData[1].answer }}</button>
+                    </div>
+                    <div class="w-100"></div>
+                    <div class="col"><button class="btn btn-primary btn-lg" style="margin: 10px" @click="onAnswer(btnData[2].correct)">{{ btnData[2].answer }}</button>
+                    </div>
+                    <div class="col"> <button class="btn btn-primary btn-lg" style="margin: 10px" @click="onAnswer(btnData[3].correct)">{{ btnData[3].answer }}</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+
 </template>
 <style>
 
