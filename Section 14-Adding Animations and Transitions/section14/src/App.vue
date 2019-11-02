130ff808251aa4a5658057c1b6bbede030f56244
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-02 07:12:13 +0000

    section14

---
+++ Section 14-Adding Animations and Transitions/section14/src/App.vue
@@ -0,0 +1,92 @@
+{<template>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <h1>Animations</h1>
+        <hr>
+        <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+        <br><br>
+        <transition name="fade">
+            <div class="alert alert-primary" role="alert" v-if="show">
+                A simple primary alert—check it out!
+            </div>
+        </transition>
+        <transition name="slide">
+            <div class="alert alert-danger" role="alert" v-if="show">
+                A simple danger alert—check it out!
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                show: false
+            }
+        }
+    }
+</script>
+
+<style>
+    .fade-enter {
+        opacity: 0;
+    }
+
+    .fade-enter-active {
+        transition: opacity 1s;
+    }
+
+    .fade-leave {
+        /*opacity: 1;*/
+    }
+
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
+    }
+
+    .slide-enter {
+        /*transform: translateY(20px);*/
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+
+    .slide-leave {
+
+    }
+
+    .slide-leave-active {
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to {
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+        }
+        to {
+            transform: translateY(20px);
+        }
+    }
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
+}
