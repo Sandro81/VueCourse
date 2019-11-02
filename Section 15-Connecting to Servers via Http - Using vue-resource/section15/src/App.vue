a073e9e7fa2578b200cf323d216bbc5929f5697b
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-02 13:57:08 +0000

    section15

---
+++ Section 15-Connecting to Servers via Http - Using vue-resource/section15/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col"> <img alt="Vue logo" src="./assets/logo.png" style="width: 50px; height: auto;">
+        </div>
+        <div class="col">
+          <p>https://github.com/pagekit/vue-resource</p>
+          <p> npm install --save vue-resource</p>
+        </div>
+        <div class="w-100"></div>
+        <div class="col">col</div>
+        <div class="col">col</div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
+
