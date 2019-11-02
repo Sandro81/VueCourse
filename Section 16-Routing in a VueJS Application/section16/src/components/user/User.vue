473b7fb0c7e2d6134f3462682233e298e6242ba8
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-02 19:01:12 +0000

    section16

--- Section 16-Routing in a VueJS Application/section16/src/components/user/User.vue
+++ Section 16-Routing in a VueJS Application/section16/src/components/user/User.vue
@@ -6,7 +6,7 @@
                     <h1>The User Page</h1>
                 </div>
                 <div class="w-100"></div>
-                <div class="col"><button @click="navigateToHome">Go to Home</button></div>
+                <div class="col"><button @click="navigateToHome" class="btn btn-primary">Go to Home</button></div>
                 <div class="col">col</div>
             </div>
         </div>
