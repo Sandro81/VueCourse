f84114b2f7769cd8a6c07da81dba7a0f8073e9c4
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-02 18:11:37 +0000

    section16

--- Section 16-Routing in a VueJS Application/section16/src/components/user/User.vue
+++ Section 16-Routing in a VueJS Application/section16/src/components/user/User.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>The User Page</h1>
+                </div>
+                <div class="w-100"></div>
+                <div class="col">col</div>
+                <div class="col">col</div>
+            </div>
+        </div>
     </div>
 </template>
 
