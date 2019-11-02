3a6e260ae1c60e1d6d051a63dfdfc3bc82ad6bcd
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-02 18:01:54 +0000

    section16

---
+++ Section 16-Routing in a VueJS Application/section16/src/router.js
@@ -0,0 +1,9 @@
+import User from "@/components/user/User";
+import Home from "@/components/Home";
+
+
+export const routes = [
+    {path: '', component: Home},
+    {path: '/user', component: User},
+
+];
