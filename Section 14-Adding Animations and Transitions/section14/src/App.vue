633818dd1b9cde9ffa1b5988a145d4f62924ec86
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-02 09:51:35 +0000

    section14

--- Section 14-Adding Animations and Transitions/section14/src/App.vue
+++ Section 14-Adding Animations and Transitions/section14/src/App.vue
@@ -107,6 +107,17 @@
                 </div>
             </div>
         </div>
+        <div class="container">
+            <div class="row">
+                <div class="col">209. Using <b>transition-group</b> - Preparations</div>
+                <div class="w-100"></div>
+                <div class="col">col</div>
+                <div class="col">col</div>
+            </div>
+        </div>
+
+
+
     </div>
 </template>
 
