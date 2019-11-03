c766bb4b731c60802c22013c52c3e9731d70e4e6
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-03 11:54:40 +0000

    section22

--- Section 22-Using Axios instead of vue-resource/section22/src/main.js
+++ Section 22-Using Axios instead of vue-resource/section22/src/main.js
@@ -5,7 +5,13 @@ import App from "@/App";
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://vuecourse-bbdb1.firebaseio.com';
+axios.defaults.headers.common['Authorization'] = 'asd token';
+axios.defaults.headers.get['Accepts'] = 'application/json';
 
+axios.interceptors.request.use(config => {
+  console.log(config);
+  return config;
+})
 
 new Vue({
   el: '#app',
