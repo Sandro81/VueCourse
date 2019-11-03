55c8b07b51398d7ed0cd4f95506a7b9a2bd398cd
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-03 12:09:49 +0000

    section22

--- Section 22-Using Axios instead of vue-resource/section22/src/axios-auth.js
+++ Section 22-Using Axios instead of vue-resource/section22/src/axios-auth.js
@@ -1,2 +1,9 @@
 import axios from 'axios';
 
+const instance = axios.create({
+    baseUrl: 'https://vuecourse-bbdb1.firebaseio.com'
+});
+
+instance.defaults.headers.common['SOMETHING'] = 'something';
+
+export default instance
