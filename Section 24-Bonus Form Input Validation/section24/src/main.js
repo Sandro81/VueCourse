a9bbb32d8d384d81194b921db1dd697e30c47679
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-03 20:50:00 +0000

    section24

---
+++ Section 24-Bonus Form Input Validation/section24/src/main.js
@@ -0,0 +1,38 @@
+import Vue from 'vue';
+import router from './router';
+import store from './store';
+import App from "@/App";
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://vuecourse-bbdb1.firebaseio.com';
+axios.defaults.headers.common['Authorization'] = 'asd token';
+axios.defaults.headers.get['Accepts'] = 'application/json';
+
+axios.interceptors.request.use(config => {
+  console.log('axios.interceptors.request.use()', config);
+  return config;
+});
+axios.interceptors.response.use(res => {
+  console.log('axios.interceptors.response.use()', res);
+  return res;
+});
+
+
+// To remove the interceptors from execution
+// axios.interceptors.request.eject(reqInterceptor);
+// axios.interceptors.response.eject(resInterceptor);
+//     const reqInterceptor = axios.interceptors.request.use(config => {
+//       console.log('axios.interceptors.request.use()', config);
+//       return config;
+//     });
+//     const resInterceptor = axios.interceptors.response.use(res => {
+//       console.log('axios.interceptors.response.use()', res);
+//       return res;
+//     });
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
