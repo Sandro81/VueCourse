460a6da8c8683cbb58445b95344c9d5536f03129
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-09 00:37:33 +0000

    vue-api

---
+++ API_VUE/api-vuejs/src/components/new-quote.vue
@@ -0,0 +1,34 @@
+<template>
+    <form @submit.prevent="onSubmitted">
+        <div class="form-group row">
+            <label for="inputPassword" class="col-sm-2 col-form-label">Content</label>
+            <div class="col-sm-10">
+                <input type="text"
+                       class="form-control"
+                       id="inputPassword"
+                       placeholder="Content"
+                       v-model="quoteContent">
+            </div>
+            <button class="btn btn-primary" type="submit">Submit</button>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                quoteContent: ''
+            }
+        },
+        methods: {
+            onSubmitted() {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
