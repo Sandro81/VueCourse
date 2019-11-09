dfbae678115b89afcda89d36fd8febe1b3cba82d
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-09 04:10:00 +0000

    vue-api

--- API_VUE/api-vuejs/src/App.vue
+++ API_VUE/api-vuejs/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <div class="container">
       <div class="row">
         <app-new-quote></app-new-quote>
@@ -12,23 +11,22 @@
 </template>
 
 <script>
-
   import NewQuote from './components/new-quote';
-export default {
-  name: 'app',
-  components: {
-    'app-new-quote': NewQuote
+  export default {
+    name: 'app',
+    components: {
+      'app-new-quote': NewQuote
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- API_VUE/api-vuejs/src/components/new-quote.vue
+++ API_VUE/api-vuejs/src/components/new-quote.vue
@@ -1,17 +1,29 @@
 <template>
-    <form @submit.prevent="onSubmitted">
-        <div class="form-group row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Content</label>
-            <div class="col-sm-10">
-                <input type="text"
-                       class="form-control"
-                       id="inputPassword"
-                       placeholder="Content"
-                       v-model="quoteContent">
+    <div>
+        <form @submit.prevent="onSubmitted">
+            <div class="form-group row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Content</label>
+                <div class="col-sm-10">
+                    <input type="text"
+                           class="form-control"
+                           id="inputPassword"
+                           placeholder="Content"
+                           v-model="quoteContent">
+                </div>
+                <button class="btn btn-primary" type="submit">Submit</button>
             </div>
-            <button class="btn btn-primary" type="submit">Submit</button>
-        </div>
-    </form>
+        </form>
+
+        <form @submit.prevent="onGet">
+            <div class="form-group row">
+                <button class="btn btn-primary" type="submit">
+                    read
+                </button>
+            </div>
+        </form>
+
+    </div>
+
 </template>
 
 <script>
@@ -24,14 +36,42 @@
         },
         methods: {
             onSubmitted() {
+
                 axios.post('http://localhost/superenglishtest/public/api/quote',
                     {content: this.quoteContent})
                     .then(
-                        (response) => console.log(response)
+                        (response) => {
+                            console.log(response.data);
+                            console.log(response.status);
+                            console.log(response.statusText);
+                            console.log(response.headers);
+                            console.log(response.config);
+                        }
                     )
                     .catch(
-                    (error) => console.log(error)
-                );
+                        (error) => console.log(error)
+                    );
+            },
+
+            onGet() {
+
+                const instance = axios.create({
+                    baseURL: 'http://localhost/superenglishtest/public/api/quotes',
+                });
+
+                instance.get()
+                    .then(
+                        (response) => {
+                            console.log(response.data);
+                            console.log(response.status);
+                            console.log(response.statusText);
+                            console.log(response.headers);
+                            console.log(response.config);
+                        }
+                    )
+                    .catch(
+                        (error) => console.log(error)
+                    );
             }
         }
     }
