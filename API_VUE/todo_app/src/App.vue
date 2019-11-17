d6e9f0fe79d50620e66917e4a79ab94e6aafb8e0
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-17 08:03:55 +0000

    https://www.youtube.com/watch?v=A5S23KS_-bU&list=PLEhEHUEU3x5q-xB1On4CsLPts0-rZ9oos&index=1

--- API_VUE/todo_app/src/App.vue
+++ API_VUE/todo_app/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -17,7 +17,17 @@ export default {
 </script>
 
 <style>
-#app {
+
+  . {
+    box-sizing: border-box;
+  }
+
+  .container {
+    max-width: 600px;
+    margin: 0 auto;
+  }
+
+  #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -25,4 +35,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+  .logo {
+    display: block;
+    margin: 20px auto;
+    height: 75px;
+  }
 </style>
