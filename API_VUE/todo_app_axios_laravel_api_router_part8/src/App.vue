51cd859550cc66a8d2e8b7bfd46f1c0ec00b745f
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-30 20:41:53 +0000

    Vue.js Todo App - Vue Router - Part 8

--- API_VUE/todo_app_axios_laravel_api_router_part8/src/App.vue
+++ API_VUE/todo_app_axios_laravel_api_router_part8/src/App.vue
@@ -28,7 +28,7 @@
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
-    margin-top: 60px;
+    /*margin-top: 60px;*/
     font-size: 24px;
   }
   .logo {
--- API_VUE/todo_app_axios_laravel_api_router_part8/src/main.js
+++ API_VUE/todo_app_axios_laravel_api_router_part8/src/main.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
+import Master from './components/layouts/Master'
 import {store} from './store/store'
-
+//npm install vue-router --save
+import VueRouter from 'vue-router'
+import routes from './routes'
 
 
 //https://www.youtube.com/watch?v=7AXiN5mrOgY&list=PLEhEHUEU3x5q-xB1On4CsLPts0-rZ9oos&index=3
 window.eventBus = new Vue();
 
 Vue.config.productionTip = false
-
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 new Vue({
-  render: h => h(App),
+  render: h => h(Master),
+  router: router,
     store: store
 }).$mount('#app')
