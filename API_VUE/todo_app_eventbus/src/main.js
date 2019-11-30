91651b23792322e43ba7c2f2d4ac7a9f8ee90795
Sandro81 <sandro.fioravanti81@gmail.com>
2019-11-30 09:40:11 +0000

    https://www.youtube.com/watch?v=7AXiN5mrOgY&list=PLEhEHUEU3x5q-xB1On4CsLPts0-rZ9oos&index=3

---
+++ API_VUE/todo_app_eventbus/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+
+
+//https://www.youtube.com/watch?v=7AXiN5mrOgY&list=PLEhEHUEU3x5q-xB1On4CsLPts0-rZ9oos&index=3
+window.eventBus = new Vue();
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
