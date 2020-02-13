f95f4ec6aa6b6aaf2a3f8cefa8482e4275077c63
Sandro81 <sandro.fioravanti81@gmail.com>
2020-02-13 05:54:37 +0000

    section14

--- API_VUE/todo_app_axios_laravel_api_router_passaport_part11/src/components/TodoItem.vue
+++ API_VUE/todo_app_axios_laravel_api_router_passaport_part11/src/components/TodoItem.vue
@@ -3,7 +3,11 @@
         <div class="todo-item-left">
             <input type="checkbox" v-model="completed" @change="doneEdit">
             <div v-if="!editing" @dblclick="editTodo" class="todo-item-label" :class="{ completed : completed }">{{ title }}</div>
-            <input v-else class="todo-item-edit" type="text" v-model="title" @blur="doneEdit" @keyup.enter="doneEdit" @keyup.esc="cancelEdit" v-focus>
+            <input v-else class="todo-item-edit"
+                   type="text" v-model="title"
+                   @blur="doneEdit"
+                   @keyup.enter="doneEdit"
+                   @keyup.esc="cancelEdit" v-focus>
         </div> <!-- end todo-item-left -->
         <div>
             <button @click="pluralize">Plural</button>
